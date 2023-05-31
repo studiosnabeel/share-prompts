4b26e5dd49f0d660ea7b10d82cedb7b0ae659b15
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-31 20:47:00 +0500

    update feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+'use client';
 
-const Feed = () => {
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data,handleTagClick}) => {
   return (
-    <div>Feed</div>
+    <div className='mt-16 prompt_layout'>
+
+    </div>
   )
 }
 
-export default Feed
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
