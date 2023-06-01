dabca027ab1e8bfa8a84f4ab7f43c936f2781184
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-01 16:50:03 +0500

    pass props to PromptCard

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,7 +7,11 @@ const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard />
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
   );
