4aa9b024777e933f78bfdb939c070377673386e3
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-01 16:48:53 +0500

    Add api endpoint for prompt and map data for card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,18 +3,32 @@
 import { useState, useEffect } from 'react';
 import PromptCard from './PromptCard';
 
-const PromptCardList = ({data,handleTagClick}) => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
-
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <section className="feed">
@@ -29,7 +43,7 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={[]} handleTagClick={() => {}} />
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
